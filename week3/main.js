cab201f47e4e699348ab6f6ffc8985a1d250c3c8
shonrob <rob19063@byui.edu>
2021-01-24 00:52:31 -0500

    update index.html for week 3 and js

--- week3/main.js
+++ week3/main.js
@@ -74,3 +74,51 @@ function reverseArrayInPlace(array) {
     reverseArrayInPlace(arrayValue);
     console.log(arrayValue);
     
+// Deep Comparison
+// Write a function deepEqual that takes two values and returns true only if 
+// they are the same value or are objects with the same properties, where the 
+// values of the properties are equal when compared with a recursive call to 
+// deepEqual.
+function deepEqual(a,b){
+    if (a === b) return true;
+    if ( a == null || typeof a != "object" || 
+        b == null || typeof b != "object") return false;
+    let keysA = Object.keys(a), 
+        keysB = Object.keys(b);
+    if (keysA.length != keysB.length) return false;
+
+    for(let key of keysA) {
+        if (!keysB.includes(key) || !deepEqual(a[key], b[key])) return false;
+    }
+    return true;
+}
+
+let obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
+
+console.log(deepEqual(obj, {here: 1, object: 2}));
+
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+ // my brain hurts.  I don't follow this one at all.
+
+//  Write a JavaScript function to get the first element of an array. 
+//  Passing a parameter 'n' will return the first 'n' elements of the 
+//  array
+function getFirst(){
+    let array = ["A","B","C","D"]
+    return array[0];
+}
+console.log(getFirst);
+//first attempt didn't work- tried without assistence
+
+var first = (array, n) => {
+    if (array == null)
+        return void 0;
+    if (n == null)
+        return array [0];
+    if (n < 0)
+        return [];
+            return array.slice(0, n);
+};
+
+console.log(first([7, 9, 0, -2]));
