3dbdc9b4d0c9fce7abc0866baa5915a1127860fa
shonrob <rob19063@byui.edu>
2021-02-20 22:14:13 -0500

    update portfolio page

--- js/main.js
+++ js/main.js
@@ -27,10 +27,12 @@ const links = [
     },
     {
         label: "week7",
-        url:"week7/index.html",
+        url:"week7/index.html"
+        
+    },
+    {
         label: "drumKitProject",
         url:"drumpKitProject/index.html"
-        
     }
   ]
 var weeks = document.getElementById("assignments");
