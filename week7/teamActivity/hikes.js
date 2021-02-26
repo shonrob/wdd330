744ed4fb5fff61bf3e0d78c810f5d14e6a838e21
shonrob <rob19063@byui.edu>
2021-02-25 20:36:25 -0500

    update files

---
+++ week7/teamActivity/hikes.js
@@ -0,0 +1,140 @@
+
+  //create an array of hikes
+const hikeList = [
+    {
+      name: "Bechler Falls",
+      imgSrc: "falls.jpg",
+      imgAlt: "Image of Bechler Falls",
+      distance: "3 miles",
+      difficulty: "Easy",
+      description:
+        "Beautiful short hike along the Bechler river to Bechler Falls",
+      directions:
+        "Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road.Drive to the end of the Cave Falls road. There is a parking area at the trailhead."
+    },
+    {
+      name: "Teton Canyon",
+      imgSrc: "falls.jpg",
+      imgAlt: "Image of Bechler Falls",
+      distance: "3 miles",
+      difficulty: "Easy",
+      description: "Beautiful short (or long) hike through Teton Canyon.",
+      directions:
+        "Take Highway 33 East to Driggs. Turn left onto Teton Canyon Road. Follow that road for a few miles then turn right onto Stateline Road for a short distance, then left onto Alta Road. Veer right after Alta back onto Teton Canyon Road. There is a parking area at the trailhead."
+    },
+    {
+      name: "Denanda Falls",
+      imgSrc: "falls.jpg",
+      imgAlt: "Image of Bechler Falls",
+      distance: "7 miles",
+      difficulty: "Moderate",
+      description:
+        "Beautiful hike through Bechler meadows river to Denanda Falls",
+      directions:
+        "Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road. Drive to until you see the sign for Bechler Meadows on the left. Turn there. There is a parking area at the trailhead."
+    }
+  ];
+  
+  const imgBasePath = "//byui-cit.github.io/cit261/examples/";
+  //on load grab the array and insert it into the page on load
+  
+  export default class Hikes {
+    constructor(elementId) {
+      this.parentElement = document.getElementById(elementId);
+      // we need a back button to return back to the list. This will build it and hide it. When we need it we just need to remove the 'hidden' class
+      this.backButton = this.buildBackButton();
+    }
+    // why is this function necessary?  hikeList is not exported, and so it cannot be seen outside of this module. I added this in case I ever need the list of hikes outside of the module. This also sets me up nicely if my data were to move. I can just change this method to the new source and everything will still work if I only access the data through this getter.
+    getAllHikes() {
+      return hikeList;
+    }
+    // For the first stretch we will need to get just one hike.
+    getHikeByName(hikeName) {
+      return this.getAllHikes().find(hike => hike.name === hikeName);
+    }
+    //show a list of hikes in the parentElement
+    showHikeList() {
+        // Refresh the parent element on each call of this method.
+        this.parentElement.innerHTML = "";
+        renderHikeList(this.parentElement, this.getAllHikes());
+        this.addHikeListener();
+        document.body.appendChild(this.backButton);
+    }
+    // show one hike with full details in the parentElement
+    showOneHike(hikeName) {
+        let hikeRef = this.getHikeByName(hikeName);
+        this.parentElement.innerHTML = "";
+        this.parentElement.appendChild(renderOneHikeFull(hikeRef));
+        this.backButton.classList.toggle('hidden');
+    }
+    // in order to show the details of a hike ontouchend we will need to attach a listener AFTER the list of hikes has been built. The function below does that.
+    addHikeListener() {
+      // We need to loop through the children of our list and attach a listener to each, remember though that children is a nodeList...not an array. So in order to use something like a forEach we need to convert it to an array.
+        let hikeElements = document.querySelectorAll("li");
+        let hikeArr = [...hikeElements];
+        hikeArr.forEach(element => {
+            let hikeName = element.querySelector("h2").innerHTML;
+            element.addEventListener('click', () => {
+                this.showOneHike(hikeName);
+            });
+        });
+    }
+    buildBackButton() {
+      const backButton = document.createElement("button");
+      backButton.classList.add('hidden');
+      backButton.addEventListener('click', () => {
+            backButton.classList.toggle('hidden');
+            this.showHikeList();
+        });
+      backButton.innerHTML = "Back to List";
+      return backButton;
+    }
+  }
+  // methods responsible for building HTML.  Why aren't these in the class?  They don't really need to be, and by moving them outside of the exported class, they cannot be called outside the module...they become private.
+  function renderHikeList(parent, hikes) {
+      hikes.forEach(hike => {
+          parent.appendChild(renderOneHikeLight(hike));
+      });
+  }
+  function renderOneHikeLight(hike) {
+    const item = document.createElement("li");
+    item.innerHTML = ` <h2>${hike.name}</h2>
+    <div class="cont">
+    <div class="image"><img src="${imgBasePath}${hike.imgSrc}" alt="${hike.imgAlt}"></div>
+    <div class="content">
+            <div>
+                <h3>Distance</h3>
+                <p>${hike.distance}</p>
+            </div>
+            <div>
+                <h3>Difficulty</h3>
+                <p>${hike.difficulty}</p>
+            </div>
+    </div>
+    </div>`;
+    return item;
+  }
+  function renderOneHikeFull(hike) {
+    const item = document.createElement("li");
+    item.innerHTML = ` <h2>${hike.name}</h2>
+    <div class="image imagefull"><img src="${imgBasePath}${hike.imgSrc}" alt="${hike.imgAlt}"></div>
+    <div class="content">
+            <div>
+                <h3>Distance</h3>
+                <p>${hike.distance}</p>
+            </div>
+            <div>
+                <h3>Difficulty</h3>
+                <p>${hike.difficulty}</p>
+            </div>
+            <div>
+                <h3>Description</h3>
+                <p>${hike.description}</p>
+            </div>
+            <div>
+                <h3>Directions</h3>
+                <p>${hike.directions}</p>
+            </div>
+    </div>`;
+    return item;
+  }
