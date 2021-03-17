6b7ea928c8cdde67b217f49ff8bbd3436234b2c1
shonrob <rob19063@byui.edu>
2021-03-16 22:04:08 -0400

    landing page on simon game

---
+++ SimonGame/main.js
@@ -0,0 +1,68 @@
+// ToDo: click on the button
+// landpage
+// save the name to variable
+// check local storage for name
+// Pull high score save in variable highScore
+//if there is no high score set to 0.
+// if exists then save score into a variable
+ //else set high score to 0 (variable yourScore)
+// 
+
+//todo:run when get started button is clicked
+function getHighScore(){
+
+}
+function getPersonalScore(){
+
+}
+function startGame(){
+
+}
+//ToDo: start game is clicked
+sequence = [];
+
+function randomGenerator(){
+    // generate numbers between 1 -5 
+    // sequence.push into array
+}
+
+function playSequence() {
+    //forEach animate and sound cooresponding with the color
+}
+
+function playerSequence(){
+    pSquence = [];
+    // when clicked add number to pSquence
+    // check pSquence === squence
+    // if true repeat else game over.
+}
+
+// ToDo: gameOver 
+function gameOver() {
+    //check to see if they got the high score
+    //if personalScore > highScore
+    //broadcast good game message your high score is..
+    //else if highScore < personalScore 
+    //great job message your highScore is this.
+ 
+}
+function checkPersonalScore() {
+
+}
+
+function checkHighScore() {
+
+}
+
+// ToDo: localStorage 
+function setLocalStorage(){
+    //check to see if name exists if so update else create new
+    //check to see if highScore exists if so update else create new
+}
+
+function resetGame(){
+    //go back to landing page.
+}
+
+
+
